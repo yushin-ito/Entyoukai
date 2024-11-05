1f203648c53228ae2698832d00b2a49857e54fcd
yushin_ito <yitengyouxin8@gmail.com>
2024-11-06 05:31:07 +0900

    fix: UIの軽微な修正

--- src/components/layouts/FAQList.tsx
+++ src/components/layouts/FAQList.tsx
@@ -26,6 +26,7 @@ const FAQList = ({ faq }: FAQListProps) => {
             alignItems="center"
             justifyContent="space-between"
             _hover={{ bg: "gray.50" }}
+            _focus={{ bg: "transparent" }}
             sx={{
               WebkitTapHighlightColor: "transparent",
             }}
