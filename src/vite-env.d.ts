105a71d27be27daab8674f4cb14e3bf7f4f9f1a5
yushin_ito <yitengyouxin8@gmail.com>
2024-11-06 17:54:51 +0900

    fix: 環境変数を読み込めるように修正

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly SUPABASE_URL: string;
-  readonly SUPABASE_KEY: string;
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_KEY: string;
 }
 
 interface ImportMeta {
