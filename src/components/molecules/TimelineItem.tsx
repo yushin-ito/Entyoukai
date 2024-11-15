d5e1554209dcf71c3753842a220058f9479ce366
yushin_ito <yitengyouxin8@gmail.com>
2024-11-15 22:30:33 +0900

    remove: 無駄な処理を削除

--- src/components/molecules/TimelineItem.tsx
+++ src/components/molecules/TimelineItem.tsx
@@ -1,43 +1,99 @@
-import { Heading, Box, Text, VStack } from "@chakra-ui/react";
+import {
+  Heading,
+  Box,
+  Text,
+  VStack,
+  HStack,
+  Circle,
+  useBreakpointValue
+} from "@chakra-ui/react";
 import { useInView } from "framer-motion";
 import { useRef, memo } from "react";
 
+import { formatByDot } from "../../functions";
 import MotionBox from "../atoms/MotionBox";
 
 type TimelineItemProps = {
-  id: string;
   title: string;
   description: string;
+  dates: string[];
 };
 
-const TimelineItem = memo(({ id, title, description }: TimelineItemProps) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const isInView = useInView(ref, { once: true });
+const TimelineItem = memo(
+  ({ title, description, dates }: TimelineItemProps) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const isInView = useInView(ref, { once: true });
+    const breakpoint = useBreakpointValue(
+      { base: "base", md: "md" },
+      { fallback: undefined }
+    );
 
-  return (
-    <Box id={id} ref={ref} w="100%">
-      <MotionBox
-        opacity={isInView ? 1 : 0}
-        initial={{ opacity: 0, x: 30 }}
-        animate={{
-          opacity: isInView ? 1 : 0,
-          x: isInView ? 0 : 30,
-          transition: { duration: 0.8 }
-        }}
-        ml={{ base: "90px", md: "180px" }}
-        rounded="md"
-      >
-        <VStack w="100%" alignItems="flex-start" spacing="8px">
-          <Heading as="h3" fontSize={{ base: "sm", md: "xl" }}>
-            {title}
-          </Heading>
-          <Text as="p" fontSize={{ base: "xs", md: "md" }}>
-            {description}
-          </Text>
+    return (
+      <HStack w="100%" spacing="4" pos="relative">
+        {/* 日付 */}
+        <VStack
+          as="time"
+          dateTime={dates[0]}
+          pos="absolute"
+          top="50%"
+          left="0"
+          transform="translateY(-50%)"
+        >
+          {dates.map((date, index) => (
+            <Text key={index} as="span" fontSize={{ base: "xs", md: "md" }}>
+              {breakpoint === "base" && formatByDot(date, "yy.M.d")}
+              {breakpoint === "md" && formatByDot(date, "yyyy.MM.dd")}
+            </Text>
+          ))}
         </VStack>
-      </MotionBox>
-    </Box>
-  );
-});
+
+        {/* 円 */}
+        <Circle
+          size={{
+            base: "12px",
+            md: "24px"
+          }}
+          bg="white"
+          pos="absolute"
+          top="50%"
+          left={{
+            base: "60px",
+            md: "120px"
+          }}
+          borderWidth={{
+            base: "2px",
+            md: "3px"
+          }}
+          borderColor="brand.500"
+          transform="translate(-50%, -50%)"
+        />
+
+        {/* コンテンツ */}
+        <Box ref={ref} w="100%">
+          <MotionBox
+            opacity={isInView ? 1 : 0}
+            initial={{ opacity: 0, x: 30 }}
+            animate={{
+              opacity: isInView ? 1 : 0,
+              x: isInView ? 0 : 30,
+              transition: { duration: 0.8 }
+            }}
+            ml={{ base: "90px", md: "180px" }}
+            rounded="md"
+          >
+            <VStack w="100%" alignItems="flex-start" spacing="12px">
+              <Heading as="h3" fontSize={{ base: "sm", md: "xl" }}>
+                {title}
+              </Heading>
+              <Text as="p" fontSize={{ base: "xs", md: "md" }}>
+                {description}
+              </Text>
+            </VStack>
+          </MotionBox>
+        </Box>
+      </HStack>
+    );
+  }
+);
 
 export default TimelineItem;
--- src/components/organisms/Timeline.tsx
+++ src/components/organisms/Timeline.tsx
@@ -1,14 +1,6 @@
-import {
-  Box,
-  VStack,
-  Text,
-  Circle,
-  useBreakpointValue
-} from "@chakra-ui/react";
-import { useState, memo, useEffect } from "react";
+import { Box, VStack } from "@chakra-ui/react";
+import { memo } from "react";
 
-import { formatByDot } from "../../functions";
-import useResize from "../../hooks/tools/useResize";
 import type { Event } from "../../types";
 import TimelineItem from "../molecules/TimelineItem";
 
@@ -17,50 +9,22 @@ type TimelineProps = {
 };
 
 const Timeline = memo(({ events }: TimelineProps) => {
-  const [circles, setCircles] = useState<number[]>([]);
-  const breakpoint = useBreakpointValue({ base: "base", md: "md" });
-  const space = useBreakpointValue(
-    { base: 50, md: 100 },
-    { fallback: undefined }
-  );
-
-  const { resize } = useResize();
-
-  useEffect(() => {
-    const circles: number[] = [];
-
-    let sum = 0;
-
-    for (let i = 0; i < events.length; i++) {
-      const element = document.getElementById(`item${i}`);
-      if (element) {
-        const rect = element.getBoundingClientRect();
-        const height = rect.height;
-        const position = sum + height / 2 + (i > 0 ? space || 0 : 0);
-
-        circles.push(position);
-        sum += height + (i > 0 ? space || 0 : 0);
-      }
-    }
-
-    setCircles(circles);
-  }, [events, events.length, space, resize]);
-
   return (
     <VStack
       as="section"
       w={{ base: "100%", md: "90%" }}
       pos="relative"
-      spacing={space}
+      spacing={{ base: "80px", md: "100px" }}
     >
+      {/* 縦線 */}
       <Box
         pos="absolute"
+        top="0"
+        bottom="0"
         left={{
           base: "60px",
           md: "120px"
         }}
-        top="0"
-        bottom="0"
         width={{
           base: "1.8px",
           md: "2.5px"
@@ -69,58 +33,13 @@ const Timeline = memo(({ events }: TimelineProps) => {
         transform="translateX(-50%)"
       />
 
-      {/* 日付 */}
-      {events.map((event, index) => (
-        <VStack
-          key={index}
-          as="time"
-          dateTime={event.dates[0]}
-          pos="absolute"
-          left="0"
-          top={`${circles[index]}px`}
-          transform="translateY(-50%)"
-        >
-          {event.dates.map((date, index) => (
-            <Text key={index} as="span" fontSize={{ base: "xs", md: "md" }}>
-              {breakpoint === "base"
-                ? formatByDot(date, "yy.M.d")
-                : formatByDot(date, "yyyy.MM.dd")}
-            </Text>
-          ))}
-        </VStack>
-      ))}
-
-      {/* 円 */}
-      {circles.map((pos, index) => (
-        <Circle
-          key={index}
-          size={{
-            base: "12px",
-            md: "24px"
-          }}
-          bg="white"
-          pos="absolute"
-          left={{
-            base: "60px",
-            md: "120px"
-          }}
-          top={`${pos}px`}
-          transform="translate(-50%, -50%)"
-          borderWidth={{
-            base: "2px",
-            md: "3px"
-          }}
-          borderColor="brand.500"
-        />
-      ))}
-
       {/* コンテンツ */}
       {events.map((event, index) => (
         <TimelineItem
           key={index}
-          id={`item${index}`}
           title={event.title}
           description={event.description}
+          dates={event.dates}
         />
       ))}
     </VStack>
