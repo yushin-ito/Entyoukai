8ae3703ad8f61a9b22a46220705a397184d74110
yushin_ito <yitengyouxin8@gmail.com>
2024-11-17 03:41:23 +0900

    add: フォントのサブセット化する処理を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+import fontSubsetter from "./plugins";
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), fontSubsetter({ verbose: true })],
   build: {
     target: "esnext",
     minify: "esbuild",
